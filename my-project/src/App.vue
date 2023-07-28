5396d22bb3e5525452642ee3a4fd397a7862e579
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-07-28 23:33:24 +0900

    싱글 파일 컴포넌트 학습

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,26 +1,32 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from "@/components/AppHeader.vue";
 export default {
-  name: 'App',
+  data() {
+    return {
+      str: 'Header'
+    }
+  },
   components: {
-    HelloWorld
+    AppHeader
+  },
+  methods: {
+    renewString() {
+      this.str = 'RENEW'
+    }
   }
 }
 </script>
 
+<template>
+  <div>
+    <AppHeader
+        v-bind:props-data="str"
+        v-on:renew="renewString"
+    />
+    {{ str }}
+  </div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ my-project/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: ['propsData'],
+  methods: {
+    sendEvent() {
+      this.$emit('renew')
+    }
+  }
+}
+
+</script>
+
+<template>
+  <header>
+    <h1>{{propsData}}</h1>
+    <button v-on:click="sendEvent">Send</button>
+  </header>
+</template>
+
+<style>
+
+</style>
