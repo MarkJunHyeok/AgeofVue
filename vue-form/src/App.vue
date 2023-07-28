8ca1376d4deb1e4eb442ad005afee0ce76685d66
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-07-29 00:23:32 +0900

    form 컴포넌트 사용으로 axios 활용

---
+++ vue-form/src/App.vue
@@ -0,0 +1,46 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username : '',
+      password : ''
+    }
+  },
+  methods: {
+    submitForm() {
+      var url = 'https://jsonplaceholder.typicode.com/users'
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+
+      axios.post(url, data)
+          .then(function (response) {
+            console.log(response)
+          }).catch(function (error) {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <lable for="username">id:</lable>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <lable for="password">password:</lable>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
